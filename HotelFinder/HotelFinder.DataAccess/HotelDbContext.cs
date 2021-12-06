d7bfd50b4910e6d76dcb2aceac05745f32aad985
erolDemirhan <erol.demirhan30@gmail.com>
2021-12-06 21:36:15 +0300

    basic api solution based on patterns

---
+++ HotelFinder/HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=EROLDEMIRHAN; Database=HotelDb; uid=sa; pwd=123456");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
